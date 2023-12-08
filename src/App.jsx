bd5f008d6cf533c1896ca1a648f5def06827b0e9
franco.recoaro <flrecoaro@gmail.com>
2023-12-08 13:38:35 +0100

    inicio app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header style={{backgroundColor: "red"}}>
+        <h1>Mi primer App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
